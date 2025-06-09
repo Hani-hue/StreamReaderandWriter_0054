50ce94f18810e6ec6a102973c47c54d97d3b2506
Hani-hue <isnahabibah8729@gmail.com>
2025-06-09 09:54:11 +0700

    ioFile_bikin fungsi main

--- ioFile.cpp
+++ ioFile.cpp
@@ -2,3 +2,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    string baris;
+
+    // operasi file dalam mode menulis
+    ofstream outfile;
+    // menunjuk ke sebuah nama file untuk membuka
+}
