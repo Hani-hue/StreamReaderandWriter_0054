b19120c657f2b225dfa1c25d8ff138dbe59cec60
Hani-hue <isnahabibah8729@gmail.com>
2025-06-09 10:45:08 +0700

    manual exception

--- manualException.cpp
+++ manualException.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; // melemparkan sebuah integer maka
+        cout << "Pertanyaan tidak akan di eksekusi" << endl;
+    }
+    catch (int a)
+    {
+        // blok ini akan di eksekusi
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+    catch (...)
+    {
+        /*jika selain integer maka block ini akan di eksekusi*/
+        cout << "Default pengecualian di eksekusi" << endl;
+    }
+    return 0;
+}
